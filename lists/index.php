5f2c4ff44c21edcf58c115cb5aa0f7d2a69ca6f7
John Paul Gabule <lucas.gabule@gmail.com>
2024-12-29 22:58:14 +0800

    combine array slice and list

--- lists/index.php
+++ lists/index.php
@@ -4,6 +4,15 @@
 // list($name, $age, $wife) = $person;
 // print_r([$name, $age, $wife]);
 
-$people = ['tom', 'dick', 'harriet', 'brenda', 'jo'];
-$middle = array_slice($people, -2, 2);
-print_r($middle);
+// $people = ['tom', 'dick', 'harriet', 'brenda', 'jo'];
+// $middle = array_slice($people, -2, 2);
+// print_r($middle);
+
+// $person = ['name' => 'fred','age' => 24, 'wife' => 'dadan'];
+// $subset = array_slice($person, 1, 2);
+// print_r($subset);
+
+# Combine array_slice() with list() to extract only some values to variables:
+$persons = ['Tom', 'Dick', 'Harriet', 'Brenda', 'Jo'];
+list($second, $third) = array_slice($persons, 1, 2);
+print_r([$second, $third]);
