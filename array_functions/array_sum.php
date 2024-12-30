3b8c1b4dee1dc256a00593cc7aeecf60b0f0aa90
John Paul Gabule <lucas.gabule@gmail.com>
2024-12-30 15:44:58 +0800

    array sum

---
+++ array_functions/array_sum.php
@@ -0,0 +1,5 @@
+<?php
+
+$scores = [98, 76, 56, 80];
+$total = array_sum($scores);
+print_r($total);
--- converting/extract.php
+++ converting/extract.php
@@ -9,13 +9,35 @@
 // extract($array, EXTR_PREFIX_ALL, 'book');
 // echo "Cover: $book_cover, Book Shape: $book_shape}, Shape: $shape" . PHP_EOL;
 
-$color = "indigo";
-$shape = "curvy";
-$floppy = "none";
-$a = compact("color", "shape", "floppy");
-print_r($a) . PHP_EOL;
+// $color = "indigo";
+// $shape = "curvy";
+// $floppy = "none";
+// $a = compact("color", "shape", "floppy");
+// print_r($a) . PHP_EOL;
 
-// or
-$names = ["color", "shape", "floppy"];
-$a = compact($names);
-print_r($a);
+// // or
+// $names = ["color", "shape", "floppy"];
+// $a = compact($names);
+// print_r($a);
+
+// $addresses = ["spam@cyberpromo.net", "abuse@example.com"];
+// while (list($key, $value) = each($addresses)) {
+//   echo "$key is $value" . PHP_EOL;
+// }
+
+# Example 5-1. Building a table with the iterator functions
+// $ages = [
+//   'Person' => "Age",
+//   'Fred' => 35,
+//   'Barney' => 30,
+//   'Tigger' => 8,
+//   'Pooh' => 40,
+//   ];
+
+// reset($ages);
+// list($c1, $c2) = each($ages);
+// echo "\n$c1 | $c2\n" . PHP_EOL;
+
+// while (list($c1, $c2) = each($ages)) {
+//   echo "$c1 | $c2" . PHP_EOL;
+// }
